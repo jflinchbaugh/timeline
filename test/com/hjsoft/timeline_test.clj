27810e02f4ebe6a928e86158dc1232ceb907d1f0
John Flinchbaugh <john@hjsoft.com>
2026-03-22 18:58:50 -0400

    readme

--- test/com/hjsoft/timeline_test.clj
+++
@@ -1,7 +0,0 @@
-(ns com.hjsoft.timeline-test
-  (:require [clojure.test :refer [deftest is testing]]
-            [com.hjsoft.timeline :as sut])) ; system under test
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))

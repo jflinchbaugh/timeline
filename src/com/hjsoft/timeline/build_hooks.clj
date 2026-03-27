8fa1f0fa9036c056d561cfb22a0c537191147c34
John Flinchbaugh <john@hjsoft.com>
2026-03-27 19:17:43 -0400

    inject cache-busting versions

--- src/com/hjsoft/timeline/build_hooks.clj
+++ src/com/hjsoft/timeline/build_hooks.clj
@@ -1,5 +1,6 @@
 (ns com.hjsoft.timeline.build-hooks
-  (:require [clojure.java.io :as io]))
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]))
 
 (defn- copy-dir [src dest]
   (let [src-file (io/file src)
@@ -11,9 +12,20 @@
           (copy-dir f target)
           (io/copy f target))))))
 
+(defn- inject-cache-buster [dest-index]
+  (let [content (slurp dest-index)
+        version (System/currentTimeMillis)
+        new-content (-> content
+                        (str/replace "css/style.css" (str "css/style.css?v=" version))
+                        (str/replace "js/main.js" (str "js/main.js?v=" version)))]
+    (spit dest-index new-content)))
+
 (defn copy-resources
-  {:shadow.build/stage :flush}
+  {:shadow.build/stage #{:compile-finish :flush}}
   [build-state & args]
   (println "Build Hook: Copying resources to target/public...")
   (copy-dir "resources/public" "target/public")
+  (when (= (:shadow.build/stage build-state) :flush)
+    (println "Build Hook: Injecting cache busters (production only)...")
+    (inject-cache-buster "target/public/index.html"))
   build-state)
